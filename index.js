9fa7662de479a93615d4d29fb85553d03488e3fb
tgoldenberg <tom@agolo.com>
2018-02-11 14:53:14 -0500

    update index

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-var pusher = new Pusher({
-  
-  cluster: 'us2',
-  encrypted: true
-});
+const Pusher = require('pusher-js');
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+app.listen(3000, function() {
+  console.log('> Server listening on port 3000...');
+
+  console.log('> Initializing Pusher...');
+  var pusher = new Pusher(process.env.PUSHER_APP_KEY);
+
+  console.log('> Subscribing to changes...');
+  var channel = pusher.subscribe('presence-node-coin')
+
+  channel.bind('tx', function(data) {
+    console.log('> New transaction: ', data.message);
+  });
+
+  var count = channel.members.count;
+  console.log('> Num connected nodes: ', count, channel.members.me, channel);
+})
