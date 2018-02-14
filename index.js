3d6f3a0b54ec86f8c9aac7881484b4056b7a91bd
Tom Goldenberg <thomasagoldenberg@gmail.com>
2018-02-14 11:04:42 -0500

    get chain to start without secret key

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-const Socket = require('pusher');
 const Client = require('pusher-js');
 const express = require('express');
 const ip = require('ip');
@@ -13,78 +12,64 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var ipAddr = ip.address();
+// get current IP address in use
+const ipAddr = ip.address();
 
-var db = flatfile.sync('/tmp/node-coin.db');
-var lastBlock = db.get('last_block');
-var firstBlock = db.get('first_block')
-var wholeChain = db.get('whole_chain');
+const PUSHER_APP_KEY = '86e36fb6cb404d67a108';
 
-var socketOptions = {
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_APP_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: 'us2',
-  encrypted: true
-};
+// pull most recent version of blockchain saved
+const db = flatfile.sync('/tmp/node-coin.db');
+const lastBlock = db.get('last_block');
+const firstBlock = db.get('first_block')
+const wholeChain = db.get('whole_chain');
 
+// create a TCP/IP server on current IP address
 const netServer = net.createServer(function(socket) {
-  socket.write('> next block: 1234');
   socket.pipe(socket);
 });
+
 netServer.listen(1337, ipAddr);
-
-var socket = new Socket(socketOptions);
-
-app.post('/pusher/auth', function(req, res) {
-  console.log('> Presence auth: ', req.params, req.body);
-  var socket_id = req.body.socket_id;
-  var channel_name = req.body.channel_name;
-  var data = { user_id: ipAddr };
-  var auth = socket.authenticate(socket_id, channel_name, data);
-  res.send(auth);
-});
 
 app.listen(3000, function() {
   console.log('> Server listening on port 3000...', ipAddr);
 
-  // initialize blockchain
+  // initialize blockchain if not saved locally
   var blockchain = typeof wholeChain === 'object' ? wholeChain : new Blockchain();
-
   db.put('firstBlock', null);
   db.put('lastBlock', null);
   db.put('wholeChain', blockchain);
 
-  console.log('> Initializing Pusher...', blockchain);
+  console.log('> Initializing blockchain...', blockchain.tail.hash);
 
-  var client = new Client(process.env.PUSHER_APP_KEY, {
+  // initialize presence channel via Pusher
+  var client = new Client(PUSHER_APP_KEY, {
     cluster: 'us2',
-    authEndpoint: 'http://localhost:3000/pusher/auth',
+    authEndpoint: 'https://pusher-presence-auth.herokuapp.com/pusher/auth',
     encrypted: true
   });
 
   console.log('> Subscribing to changes...');
   var channel = client.subscribe('presence-node-coin');
 
+  // called when the client successfully joins group
   channel.bind('pusher:subscription_succeeded', function (members) {
     console.log('> Subscription succeeded: ', members);
     let hitMe = false;
     channel.members.each(function(member) {
       if (hitMe) {
-
         // found next IP address - set up server to listen and send messages
+        const netClient = new net.Socket();
+        netClient.connect(1337, member.id, function() {
+          netClient.write(blockchain.tail.hash);
+        });
+        netClient.on('data', function(data) {
+          console.log('> Received: ', data.toString());
+        })
       }
       if (member.id === ipAddr) {
         hitMe = true;
       }
     });
-    // pick an IP address to connect to
-
-    // socket.trigger('presence-node-coin', 'blocks:request_blocks', {
-    //   ip_addr: ipAddr,
-    //   last_block: blockchain.tail,
-    //   timestamp: new Date().valueOf(),
-    // });
   });
 
   channel.bind('pusher:member_added', function(member){
@@ -95,8 +80,7 @@ app.listen(3000, function() {
         // found next IP address - set up server to listen and send messages
         const netClient = new net.Socket();
         netClient.connect(1337, member.id, function() {
-          console.log('> Connected');
-          netClient.write(blockchain.tail.hash);
+          console.log('> connected to new member: ', member.id);
         });
         netClient.on('data', function(data) {
           console.log('> Received: ', data.toString());
