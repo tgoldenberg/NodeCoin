ddaf2a10d47bdc9925ba54525d10d8291df60008
Tom Goldenberg <thomasagoldenberg@gmail.com>
2018-03-05 20:53:11 -0500

    update service

--- index.js
+++ index.js
@@ -24,36 +24,40 @@ const db = flatfile.sync('/tmp/node-coin.db');
 const wholeChain = db.get('whole_chain');
 const coinAddress = db.get('public_address');
 
+function handleConnection(conn) {
+  const remoteAddr = `${conn.remoteAddress}:${conn.remotePort}`;
+  console.log(`> new client connection from ${remoteAddr}`);
+
+  conn.on('data', onConnData);
+  conn.on('close', onConnClose);
+  conn.on('error', onConnError);
+
+  function onConnData(d) {
+    console.log(`> Connection data from: ${remoteAddr}`, d);
+    conn.write(d);
+  }
+  function onConnClose() {
+    console.log('connection from %s closed', remoteAddr);
+  }
+  function onConnError(err) {
+    console.log('Connection %s error: %s', remoteAddr, err.message);
+  }
+}
+
 app.listen(process.env.PORT || 3000, function() {
   console.log('> Server listening on port ', process.env.PORT, ipAddr);
+
   // create a TCP/IP server on current IP address
-  const netServer = net.createServer(function(socket) {
-    socket.pipe(socket);
+  const server = net.createServer();
+  server.on('connection', handleConnection);
+
+  server.listen(8334, function() {
+    console.log(`> Server listening to : ${JSON.stringify(server.address())}`);
   });
-
-  netServer.listen(1337, ipAddr);
-
-  // check every 10 minutes if IP address has changes
-  setInterval(function() {
-    let newIpAddr = ip.address();
-    if (newIpAddr !== ipAddr) {
-      console.log('> Update IP address: ', newIpAddr);
-    }
-  }, 10 * 1000 * 60);
-
-  // initialize blockchain if not saved locally
-  var blockchain = new Blockchain(wholeChain);
-  db.put('wholeChain', blockchain);
-
-  console.log('> Initializing blockchain...', blockchain.transactions[blockchain.transactions.length - 1].hash);
 
   // initialize presence channel via Pusher
   var client = new Client(PUSHER_APP_KEY, {
-    auth: {
-      params: {
-        ip_addr: ipAddr,
-      }
-    },
+    auth: { params: { ip_addr: ipAddr, port: 8334 } },
     cluster: 'us2',
     authEndpoint: 'https://pusher-presence-auth.herokuapp.com/pusher/auth',
     encrypted: true
@@ -67,84 +71,14 @@ app.listen(process.env.PORT || 3000, function() {
     console.log('> Subscription succeeded: ', members);
     let hitMe = false;
     channel.members.each(function(member) {
-      // if (hitMe) {
-      //   // found next IP address - set up server to listen and send messages
-      //   const netClient = new net.Socket();
-      //   netClient.connect(1337, member.id, function() {
-      //     netClient.write(blockchain.tail.hash);
-      //   });
-      //   netClient.on('data', function(data) {
-      //     console.log('> Received: ', data.toString());
-      //     // TODO: take last 50 blocks and add to blockchain
-      //   });
-      //   hitMe = false;
-      // }
-      // if (member.id === ipAddr) {
-      //   hitMe = true;
-      // }
+      // pick member and ask for block headers
+      console.log('> Member: ', member);
     });
-
-    let publicAddress = process.argv[2];
-    console.log('> Current addresses: ', blockchain.addresses);
-    if (publicAddress) {
-      if (_.has(blockchain.addresses, publicAddress)) {
-        blockchain.me = publicAddress;
-      } else {
-        throw new Error('Provided address is not registered.');
-      }
-    } else {
-      if (coinAddress) {
-        if (_.has(blockchain.addresses, coinAddress)) {
-          blockchain.me = coinAddress;
-        } else {
-          throw new Error('Provided address is not registered.');
-        }
-      } else {
-        // generate new address and confirm in blockchain
-        let address = createAddress('BTC');
-        let block = blockchain.registerAddress(address);
-        // save address to Blockchain
-        blockchain.me = address.publicAddress;
-        // broadcast new block
-        channel.members.each(function(member) {
-          // found next IP address - set up server to listen and send messages
-          if (member.id !== ipAddr) {
-            // const netClient = new net.Socket();
-            // netClient.connect(1337, member.id, function() {
-            //   console.log('> Connected to: ', member.id);
-            //   netClient.write('> New block: ' + JSON.stringify(block));
-            // });
-            // netClient.on('data', function(data) {
-            //   console.log('> Received: ', data.toString());
-            //   // compare with current blockchain and set whichever is longest
-            // });
-          }
-        });
-      }
-    }
-    console.log('> My address: ', blockchain.me);
   });
 
   channel.bind('pusher:member_added', function(member){
     console.log('> Member added: ', member);
-    let hitMe = false;
-    channel.members.each(function(member) {
-      // if (hitMe) {
-        // found next IP address - set up server to listen and send messages
-      //   const netClient = new net.Socket();
-      //   netClient.connect(1337, member.id, function() {
-      //     console.log('> connected to new member: ', member.id);
-      //   });
-      //   netClient.on('data', function(data) {
-      //     console.log('> Received: ', data.toString());
-      //     netClient.write('> Latest chain: ' + blockchain.tail.hash);
-      //   });
-      //   hitMe = false;
-      // }
-      // if (member.id === ipAddr) {
-      //   hitMe = true;
-      // }
-    });
+    // send ping to new member to exchange headers
   });
 
   channel.bind('pusher:member_removed', function(member){
